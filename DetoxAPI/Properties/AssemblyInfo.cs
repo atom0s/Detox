d6cb7a142ba592078a64c2b708b47ea7c9f87610
atom0s <atom0s@live.com>
2013-11-20 10:46:09 -0800

    Adjusted file information.

--- DetoxAPI/Properties/AssemblyInfo.cs
+++ DetoxAPI/Properties/AssemblyInfo.cs
@@ -1,16 +1,33 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// -- Detox License ------------------------------------------------------
+//
+//    This file is part of Detox.
+//
+//    Detox is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Detox is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Detox.  If not, see <http://www.gnu.org/licenses/>.
+// -----------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DetoxAPI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Detox API library.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("atom0s")]
 [assembly: AssemblyProduct("DetoxAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © atom0s 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
